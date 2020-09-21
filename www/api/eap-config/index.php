5be37cabb0d4f03a559b4c53fa17ec9e852e774c
Jørn Åne de Jong <jorn.dejong@uninett.no>
2020-09-21 10:52:46 +0200

    Workarounds for bugs in the ionic app

--- www/api/eap-config/index.php
+++ www/api/eap-config/index.php
@@ -6,7 +6,9 @@ if (strpos($_SERVER['QUERY_STRING'], '?')) {
 
 require implode(DIRECTORY_SEPARATOR, [dirname(__DIR__, 3), 'src', '_autoload.php']);
 
-/* The current ionic app uses GET here, so allow for now
+// The old ionic app uses GET here, so allow for now to keep compatibility
+// The current ionic app does this OK, so no issue link
+/*
 if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
 	header( 'Content-Type: text/plain', true, 405 );
 	die( "405 Method Not Allowed\r\n\r\nOnly POST is allowed for this resource\r\n" );
@@ -18,8 +20,15 @@ $app->registerExceptionHandler();
 $realm = $app->getRealm();
 $oauth = $app->getOAuthHandler( $realm );
 $token = $oauth->getAccessTokenFromRequest( 'eap-metadata' );
-$user = new letswifi\realm\User( $token->getGrant()->getSub() );
+$grant = $token->getGrant();
+$user = new letswifi\realm\User( $grant->getSub() );
 $generator = $realm->getUserEapConfig( $user );
 $payload = $generator->generate();
+
+// Hack, https://github.com/geteduroam/ionic-app/issues/31
+if ( $grant->getClientId() === '07dc14f4-62d1-400a-a25b-7acba9bd7773' ) {
+	$payload = str_replace( '<ClientCertificate format="PKCS12" encoding="base64">', '<ClientCertificate>', $payload );
+}
+
 header( 'Content-Type: ' . $generator->getContentType() );
 echo $payload;
