27667f923a8102e060e768cc0a9a27b0d6013756
Paul Dekkers <paul.dekkers@surf.nl>
2021-04-20 13:47:02 +0200

    Correct NULL timestamp for revocation

--- sql/letswifi.mysql.sql
+++ sql/letswifi.mysql.sql
@@ -52,7 +52,7 @@ CREATE TABLE `realm_signing_log` (
 		`expires` timestamp NOT NULL,
 		`csr` blob NOT NULL,
 		`x509` blob DEFAULT NULL,
-		`revoked` timestamp DEFAULT NULL,
+		`revoked` timestamp NULL DEFAULT NULL,
 		`usage` enum('client','server') COLLATE utf8mb4_unicode_ci NOT NULL,
 		`client` varchar(127) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 		`user_agent` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
