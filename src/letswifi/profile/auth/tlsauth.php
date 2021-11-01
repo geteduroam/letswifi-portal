0d06439d4ede3e4a985a741bfbbceaa1a05e3c91
Jørn Åne de Jong <jornane.dejong@surf.nl>
2021-11-01 15:59:19 +0100

    s/jorn.dejong@uninett.no/jornane.dejong@surf.nl/g

--- src/letswifi/profile/auth/tlsauth.php
+++ src/letswifi/profile/auth/tlsauth.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of letswifi; a system for easy eduroam device enrollment
  *
- * Copyright: 2018-2021, Jørn Åne de Jong, Uninett AS <jorn.dejong@uninett.no>
+ * Copyright: 2018-2021, Jørn Åne de Jong, Uninett AS <jornane.dejong@surf.nl>
  * Copyright: 2020-2021, Paul Dekkers, SURF <paul.dekkers@surf.nl>
  * SPDX-License-Identifier: BSD-3-Clause
  */
