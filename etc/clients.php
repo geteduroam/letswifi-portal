bfb62009687893a01d2a0817e2739e9c33de3d99
Jørn Åne de Jong <jorn.dejong@letswifi.eu>
2021-12-31 11:51:09 +0100

    Use indexed array for included clients
    The change is cosmetic, but it allows administrators to easily add extra
    clients in the configuration file, with:
    
    	include 'clients.php' + [[…], […], …]
    
    This works because the `+` operator merges arrays but removes duplicate
    array keys.  Since the included clients have their client_id as key,
    it will never conflict with a client defined by the administrator,
    even if the administrator does not use keys and thus the keys are set
    automatically to incrementing integers.

--- etc/clients.php
+++ etc/clients.php
@@ -1,6 +1,6 @@
 <?php return [
 	# Shell script test application
-	[
+	'app.geteduroam.sh' => [
 		'clientId' => 'app.geteduroam.sh',
 		// We use mainly IPv4 because the nc binary might not use IPv6 by default
 		'redirectUris' => ['http://[::1]/', 'http://127.0.0.1/'],
@@ -10,7 +10,7 @@
 
 	# Windows application
 	# https://github.com/geteduroam/windows-app
-	[
+	'app.geteduroam.win' => [
 		'clientId' => 'app.geteduroam.win',
 		// Windows supports IPv6 just fine, so the IPv4 version might not be needed
 		'redirectUris' => ['http://[::1]/', 'http://127.0.0.1/'],
@@ -20,7 +20,7 @@
 
 	# Mobile application
 	# https://github.com/geteduroam/ionic-app/pull/47
-	[
+	'app.eduroam.geteduroam' => [
 		'clientId' => 'app.eduroam.geteduroam',
 		'redirectUris' => ['app.eduroam.geteduroam:/'],
 		'scopes' => ['eap-metadata']
