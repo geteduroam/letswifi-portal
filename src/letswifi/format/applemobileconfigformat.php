83103b542a02973a25e839ad0a0f6863fb8fc6ae
Jason Healy <jhealy@suffieldacademy.org>
2026-07-15 14:31:44 -0400

    Changed CA to true; without it macOS won't trust certs that don't have an existing chain of trust.

--- src/letswifi/format/applemobileconfigformat.php
+++ src/letswifi/format/applemobileconfigformat.php
@@ -34,9 +34,10 @@ class AppleMobileconfigFormat extends Format
 
 		$caCertificates = $this->credential->realm->getTrustedCACertificates();
 
-		// If we include the CA, MacOS will also trust that CA for HTTPS traffic
+		// MacOS won't use the client cert unless there is a full
+		// chain of trust, so include the CA
 		// MacOS needs the bundle to be 3DES encoded
-		$pkcs12 = $this->credential->getPKCS12( ca: false, des: true );
+		$pkcs12 = $this->credential->getPKCS12( ca: true, des: true );
 
 		$result = '<?xml version="1.0" encoding="UTF-8"?>'
 			. "\n" . '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
