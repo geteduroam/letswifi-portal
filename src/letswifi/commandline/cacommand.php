77b1aafbd5b41f63d6aca1dc83d97f18547498c7
Jørn Åne de Jong <jorn.dejong@letswifi.eu>
2026-09-16 18:58:22 +0200

    Add commandline for listing and exporting certificates

--- src/letswifi/commandline/cacommand.php
+++ src/letswifi/commandline/cacommand.php
@@ -11,17 +11,22 @@
 namespace letswifi\commandline;
 
 use Exception;
+use fyrkat\configmap\ConfigurationException;
 use fyrkat\openssl\PrivateKey;
 use fyrkat\openssl\X509;
 
 class CACommand extends Command
 {
 	public const HELP = [
+		'' . self::BOLD . 'list' . self::NORMAL . '',
+		'' . self::BOLD . 'get' . self::NORMAL . ' <common-name>',
 		'' . self::BOLD . 'create' . self::NORMAL . ' <common-name>',
 		'' . self::BOLD . 'import' . self::NORMAL . '',
 	];
 
 	public const LONG_HELP = [
+		'List the certificates that are currently available',
+		'Export a certificate as PEM, without private key matertial',
 		'Create a new certificate with private key, with the provided common name.',
 		'Import an existing certificate and (optionally) private key, read from stdin.',
 	];
@@ -29,21 +34,60 @@ class CACommand extends Command
 	public function run(): void
 	{
 		switch ( $this->argv[1] ?? '' ) {
+			case 'list':
+				$this->listCertificates();
+
+				exit( 0 );
+
 			case 'import':
 				$this->importCertificates();
-				break;
+
+				exit( 0 );
+
+			case 'get':
+				if ( !( $this->argv[2] ?? '' ) ) {
+					break;
+				}
+				$this->getCertificate( $this->argv[2] );
+
+				exit( 0 );
+
 			case 'create':
-				if ( $this->argv[2] ?? '' ) {
-					static::die( 2, 'No common-name provided' );
+				if ( !( $this->argv[2] ?? '' ) ) {
+					break;
 				}
 				$this->createSigningCertificate( $this->argv[2] );
-				break;
 
-			default:
-				$helpCommand = new HelpCommand( [$this->argv[0], $this->getName()] );
-				$helpCommand->run();
+				exit( 0 );
+		}
+		$helpCommand = new HelpCommand( [$this->argv[0], $this->getName()] );
+		$helpCommand->run();
 
-				exit( 2 );
+		exit( 2 );
+	}
+
+	protected function listCertificates(): void
+	{
+		// TODO show issuer using a flag
+		$certificates = $this->config->getDictionary( 'certificate' );
+		$table = new Table( 'common_name', /* 'issuer', */ 'private_key' );
+		foreach ( $certificates as $cn => $data ) {
+			$table->add(
+				$cn,
+				// $data['issuer'] ?? '-',
+				null === $data['key'] ? 'no' : 'yes',
+			);
+		}
+
+		echo $table->printTable( margin: 0, header: true );
+	}
+
+	protected function getCertificate( string $commonName ): void
+	{
+		try {
+			echo $this->config->getDictionary( 'certificate' )->getDictionary( $commonName )->getString( 'x509' );
+		} catch ( ConfigurationException $e ) {
+			static::die( 4, $e->getMessage() );
 		}
 	}
 
@@ -51,7 +95,7 @@ class CACommand extends Command
 	{
 		$certificateConfig = $this->config->getDictionary( 'certificate' );
 		$stdin = \file_get_contents( 'php://stdin' ) ?: '';
-		\preg_match_all( '/(?:^|\\R)-----BEGIN(?: (EC|RSA))? PRIVATE KEY-----\\R.*?\\R-----END(?: \1)? PRIVATE KEY-----(?:$|\\R)/sm', $stdin, $keys );
+		\preg_match_all( '/(?:^|\\R)-----BEGIN(?: (EC|RSA))? PRIVATE KEY-----\\R.*?\\R-----END(?: \\1)? PRIVATE KEY-----(?:$|\\R)/sm', $stdin, $keys );
 		\preg_match_all( '/(?:^|\\R)-----BEGIN CERTIFICATE-----\\R.*?\\R-----END CERTIFICATE-----(?:$|\\R)/sm', $stdin, $certificates );
 
 		$keys = \array_map( static fn( string $key ) => new PrivateKey( $key ), $keys[0] );
